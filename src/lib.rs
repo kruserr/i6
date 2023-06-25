598ae5504ac44d6046d6ab918b45bf1563d97409
kruserr <46799551+kruserr@users.noreply.github.com>
2023-06-25 20:27:25 +0000

    chore(release): prepare for v0.1.5

--- src/lib.rs
+++ src/lib.rs
@@ -13,23 +13,23 @@
 //!     <img src="https://img.shields.io/github/v/release/kruserr/i6?sort=semver&logo=GitHub&logoColor=white">
 //!   </a>
 //!   <a href="https://crates.io/crates/i6" target="_blank">
-//!     <img src="https://img.shields.io/crates/v/i6?logo=Rust&logoColor=white"/> 
+//!     <img src="https://img.shields.io/crates/v/i6?logo=Rust&logoColor=white"/>
 //!   </a>
 //!   <br/>
 //!   <a href="https://hub.docker.com/r/kruserr/i6" target="_blank">
-//!     <img src="https://img.shields.io/docker/v/kruserr/i6?sort=semver&logo=docker&logoColor=white"/> 
+//!     <img src="https://img.shields.io/docker/v/kruserr/i6?sort=semver&logo=docker&logoColor=white"/>
 //!   </a>
-//!   <a href="https://codecov.io/gh/kruserr/i6" target="_blank"> 
-//!     <img src="https://img.shields.io/codecov/c/gh/kruserr/i6?logo=Codecov&logoColor=white"/> 
+//!   <a href="https://codecov.io/gh/kruserr/i6" target="_blank">
+//!     <img src="https://img.shields.io/codecov/c/gh/kruserr/i6?logo=Codecov&logoColor=white"/>
 //!   </a>
 //! </p>
-//! 
+//!
 //! # i6
 //! A collection of tools
-//! 
+//!
 //! ## Documentation
 //! Visit the [Documentation](https://docs.rs/i6).
-//! 
+//!
 //! ## Getting Started
 //! ### Cargo
 //! Install and run with cargo
@@ -37,13 +37,13 @@
 //! cargo install --locked i6
 //! i6
 //! ```
-//! 
+//!
 //! ### Docker
 //! Run with docker
 //! ```bash
 //! docker run -it --rm --name i6 kruserr/i6:0.1
 //! ```
-//! 
+//!
 //! ### Git and cargo
 //! Clone the repo, build from source and run
 //! ```bash
