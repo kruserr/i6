e45946c0931f291dba6949a04dc1ffb38d1ec9ba
kruserr <46799551+kruserr@users.noreply.github.com>
2024-10-11 15:05:59 +0000

    ci: update tooling

--- tooling/ci.sh
+++ tooling/ci.sh
@@ -2,14 +2,15 @@
 set -Eeuo pipefail
 
 ci () {
+  cargo audit
+  cargo upgrade --verbose
+  cargo update --verbose
+
   cargo +nightly fmt --all
   cargo clippy --all-targets --all-features -- -Dwarnings
   cargo test
 
-  # cargo audit
-  # cargo upgrade --verbose
-  # cargo update --verbose
-  # cargo +nightly udeps --all-targets
+  cargo +nightly udeps --all-targets
 }
 
 ci
